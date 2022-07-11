bc1ff1db43f2c6116c4a619b987038fce56033f5
sam neat <samjneat@gmail.com>
2022-07-11 11:40:03 +0100

    increased size of paddles, background colour changes over time

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ function update(time) {
     const delta = time - lastTime
     ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()])
     computerPaddle.update(delta, ball.y)
+    const hue =parseFloat(getComputedStyle(document.documentElement).getPropertyValue("--hue"))
+
+    document.documentElement.style.setProperty("--hue", hue + delta * 0.01)
 
     if (isLose()) handleLose()
   }

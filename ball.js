d0b6ca2298d8eb93093c2dfc8dabc4941dffd1c8
sam neat <samjneat@gmail.com>
2022-07-11 11:35:45 +0100

    score increments when player or computer miss ball, both paddles deflect ball

--- ball.js
+++ ball.js
@@ -38,22 +38,31 @@ export default class Ball {
     this.velocity = INITIAL_VELOCITY
   }
 
-  update(delta) {
+  update(delta, paddleRects) {
     this.x += this.direction.x * this.velocity * delta
     this.y += this.direction.y * this.velocity * delta
     this.velocity += VELOCITY_INCREASE * delta
     const rect = this.rect()
 
-    if(rect.right >= window.innerWidth || rect.left <= 0) {
-      this.direction.x *= -1
-    }
-
     if(rect.bottom >= window.innerHeight || rect.top <= 0) {
       this.direction.y *= -1
+    }
+
+    if(paddleRects.some(r => isCollision(r, rect))) {
+      this.direction.x *= -1
     }
   }
 }
 
 function randomNumberBetween(min, max) {
   return Math.random() * (max - min) + min
+}
+
+function isCollision(rect1, rect2) {
+  return (
+    rect1.left <= rect2.right &&
+    rect1.right >= rect2.left &&
+    rect1.top <= rect2.bottom &&
+    rect1.bottom >= rect2.top
+  )
 }
